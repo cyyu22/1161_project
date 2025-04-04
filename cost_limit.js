fa69cc1ff6146cbbd630204e788bf7304fbc2604
HojoonM77 <hojoon77@my.yorku.ca>
2025-04-03 21:56:30 -0400

    Add files via upload

--- cost_limit.js
+++ cost_limit.js
@@ -1,85 +1,92 @@
-let daycost = 0;
-let monthcost = 0;
-let dayLimit;
-let monthLimit;
 
-let costs_organized_month = {
-    "Groceries":[],
-    "Food":[],
-    "Clothes":[],
-    "Entertainment":[],
-    "Rent":[],
-    "Transport":[],
-    "Other":[]
-}
-
-let costs_organized_day = {
-    "Groceries":[],
-    "Food":[],
-    "Clothes":[],
-    "Entertainment":[],
-    "Rent":[],
-    "Transport":[],
-    "Other":[]
-}
-
-let cost_update = document.getElementById("update_daily_costs");
-document.addEventListener("DOMContentLoaded", cost_update.onclick = costs());
 
 function costs() {
-    daycost += Number(document.getElementById("Cost today"));
-    if ((daycost >= (dayLimit * 0.8)) && (dayLimit != 0)) {
+    if (localStorage.getItem("daycost") != null) {
+        localStorage.setItem("daycost", localStorage.getItem(daycost) + Number(document.getElementById("Cost today")));
+    }
+    else {
+        localStorage.setItem("daycost", Number(document.getElementById("Cost today")));
+        localStorage.setItem("Groceries", 0)
+    }
+    if ((localStorage.getItem("daycost") >= (localStorage.getItem("dayLimit") * 0.8)) && (localStorage.getItem("dayLimit") != 0)) {
         limit_warning(false);
     }
-    monthcost += Number(document.getElementById("Cost today"));
-    if ((monthcost >= (monthLimit * 0.8)) && (monthLimit != 0)) {
+    if (localStorage.getItem("monthcost") != null) {
+        localStorage.setItem("monthcost", localStorage.getItem("monthcost") + Number(document.getElementById("Cost today")))
+    }
+    else {
+        localStorage.setItem("monthcost", Number(document.getElementById("Cost today")));
+    }
+    if ((localStorage.getItem("monthcost") >= (localStorage.getItem("monthLimit") * 0.8)) && (localStorage.getItem("monthLimit") != 0)) {
        limit_warning(true);
     }
 }
 
 function limit_setter()
 {
-    dayLimit = Number(document.getElementById("dailyLimit"));
-    monthLimit = Number(document.getElementById("monthlyLimit"));
-    alert(input)
+    localStorage.setItem("dayLimit", Number(document.getElementById("dailyLimit")));
+    localStorage.setItem("monthLimit", Number(document.getElementById("monthlyLimit")));
 }
 
 function limit_warning(sev) {
     if (sev) {
-        window.alert("Warning! You are approaching the montly cost limit!");
+        window.alert("Warning! You are approaching the monthly cost limit!");
     }
     else {
-        window.alert("Warning! You are approaching the cost daily limit!");
+        window.alert("Warning! You are approaching the daily cost limit!");
     }
 }
 
 const isToday = (someDate) => {
     const today = new Date()
-    return someDate.getDate() == today.getDate();
+    return someDate.getDate() == today.getDate() &&
+    someDate.getMonth() == today.getMonth() &&
+    someDate.getFullYear() == today.getFullYear()
 }
 
+const isMonth = (someDate) => {
+    const today = new Date()
+    return someDate.getMonth() == today.getMonth() &&
+    someDate.getFullYear() == today.getFullYear()
+}
+  
 window.onload = function init() {
-    const {value, date} = JSON.parse(localStorage.getValue('key'));
-
+    const { value, date } = JSON.parse(localStorage.getItem("daycost"));
+    const { Mvalue, Mdate} = JSON.parse(localStorage.getItem("monthcost"))
+      
     if (!isToday(date)) {
-        localStorage.setValue('key', JSON.stringify({ date: new Date(), daycost: 0}));
+        localStorage.removeItem("daycost");
+        localStorage.setItem("GroceriesDay", 0);
+        localStorage.setItem("FoodDay", 0);
+        localStorage.setItem("ClothesDay", 0);
+        localStorage.setItem("EntertainmentDay", 0);
+        localStorage.setItem("RentDay", 0);
+        localStorage.setItem("TransportDay", 0);
+        localStorage.setItem("OtherDay", 0);
     }
 
+    if (!isMonth(Mdate)) {
+        localStorage.removeItem("monthcost");
+        localStorage.setItem("GroceriesMonth", 0);
+        localStorage.setItem("FoodMonth", 0);
+        localStorage.setItem("ClothesMonth", 0);
+        localStorage.setItem("EntertainmentMonth", 0);
+        localStorage.setItem("RentMonth", 0);
+        localStorage.setItem("TransportMonth", 0);
+        localStorage.setItem("OtherMonth", 0);
+    }
 }
 
 function cost_organizer() {
     var cat = document.getElementsByName('costCategory');
     for (i = 0; i < cat.length; i++) {
         if (cat[i].checked) {
-            costs_organized_day[cat[i]].push(document.getElementById("Cost today"));
-            costs_organized_month[cat[i]].push(document.getElementById("Cost today"));
+            localStorage.setItem(cat[i] + "Day", localStorage.getItem(cat[i] + "Day") + document.getElementById("Cost today"));
+            localStorage.setItem(cat[i] + "Month", localStorage.getItem(cat[i] + "Month") + document.getElementById("Cost today"));
         }
     }
 }
 
-function reportgen() {
-    cost_report.html.onload = report()
-}
 
 function report() {
 
